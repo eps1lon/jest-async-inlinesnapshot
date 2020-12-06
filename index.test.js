534901e63c0585f6389993ecc6652b7c58888c7d
eps1lon <silbermann.sebastian@gmail.com>
2020-12-06 17:11:13 +0100

    Better example

--- index.test.js
+++ index.test.js
@@ -1,21 +1,32 @@
 const { toMatchSnapshot, toMatchInlineSnapshot } = require("jest-snapshot");
 
+async function observe(fn) {
+  const start = performance.now();
+  const result = await fn();
+
+  return {
+    // Arbitrary, stable value that observes some global value during `await fn()`.
+    slow: performance.now() - start > Number.EPSILON,
+    result,
+  };
+}
+
 expect.extend({
   async toMatchSpeechInlineSnapshotAsync(fn, ...rest) {
     // Jest uses the stacktraces to find the callsite.
     // For async stacktraces it'll point to the wrapped call
     // i.e. every `toMatchSpeechInlineSnapshotAsync` will point to the same call triggering "Jest: Multiple inline snapshots for the same call are not supported."
     this.error = new Error();
-    const result = await fn();
-    return toMatchInlineSnapshot.call(this, result, ...rest);
+    const observation = await observe(fn);
+    return toMatchInlineSnapshot.call(this, observation, ...rest);
   },
   toMatchSpeechInlineSnapshot(fn, ...args) {
     const result = fn();
     return toMatchInlineSnapshot.call(this, result, ...args);
   },
   async toMatchSpeechSnapshotAsync(fn, ...args) {
-    const result = await fn();
-    return toMatchSnapshot.call(this, result, ...args);
+    const observation = await observe(fn);
+    return toMatchSnapshot.call(this, observation, ...args);
   },
   toMatchSpeechSnapshot(fn, ...args) {
     const result = fn();
@@ -59,9 +70,19 @@ describe("custom inline snapshot matcher", () => {
   it("works if async", async () => {
     await expect(async () => {
       return "async inline snapshot #1";
-    }).toMatchSpeechInlineSnapshotAsync(`"async inline snapshot #1"`);
+    }).toMatchSpeechInlineSnapshotAsync(`
+            Object {
+              "result": "async inline snapshot #1",
+              "slow": true,
+            }
+          `);
     await expect(async () => {
       return "async inline snapshot #2";
-    }).toMatchSpeechInlineSnapshotAsync(`"async inline snapshot #2"`);
+    }).toMatchSpeechInlineSnapshotAsync(`
+            Object {
+              "result": "async inline snapshot #2",
+              "slow": true,
+            }
+          `);
   });
 });
