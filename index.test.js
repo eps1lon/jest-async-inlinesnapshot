64169477228ffed55633aa2a85a311884f3088be
eps1lon <silbermann.sebastian@gmail.com>
2020-12-06 16:52:41 +0100

    Initial commit

---
+++ index.test.js
@@ -0,0 +1,82 @@
+const { toMatchSnapshot, toMatchInlineSnapshot } = require("jest-snapshot");
+
+async function toMatchSpeechInlineSnapshotAsync(fn, expectedSnapshot) {
+  // Jest uses the stacktraces to find the callsite.
+  // For async stacktraces it'll point to the wrapped call
+  // i.e. every `toMatchSpeechInlineSnapshotAsync` will point to the same call triggering "Jest: Multiple inline snapshots for the same call are not supported."
+  this.error = new Error();
+  const result = await fn();
+  if (expectedSnapshot === undefined) {
+    return toMatchInlineSnapshot.call(this, result);
+  }
+  return toMatchInlineSnapshot.call(this, result, expectedSnapshot);
+}
+
+expect.extend({
+  async toMatchSpeechInlineSnapshotAsync(fn, expectedSnapshot) {
+    // Jest uses the stacktraces to find the callsite.
+    // For async stacktraces it'll point to the wrapped call
+    // i.e. every `toMatchSpeechInlineSnapshotAsync` will point to the same call triggering "Jest: Multiple inline snapshots for the same call are not supported."
+    this.error = new Error();
+    const result = await fn();
+    if (expectedSnapshot === undefined) {
+      return toMatchInlineSnapshot.call(this, result);
+    }
+    return toMatchInlineSnapshot.call(this, result, expectedSnapshot);
+  },
+  toMatchSpeechInlineSnapshot(fn, ...args) {
+    const result = fn();
+    return toMatchInlineSnapshot.call(this, result, ...args);
+  },
+  async toMatchSpeechSnapshotAsync(fn, ...args) {
+    const result = await fn();
+    return toMatchSnapshot.call(this, result, ...args);
+  },
+  toMatchSpeechSnapshot(fn, ...args) {
+    const result = fn();
+    return toMatchSnapshot.call(this, result, ...args);
+  },
+});
+
+describe("custom snapshot matcher", () => {
+  it("works if sync", () => {
+    expect(() => {
+      return "sync snapshot #1";
+    }).toMatchSpeechSnapshot();
+
+    expect(() => {
+      return "sync snapshot #2";
+    }).toMatchSpeechSnapshot();
+  });
+
+  it("works if async", async () => {
+    await expect(async () => {
+      return "async snapshot #1";
+    }).toMatchSpeechSnapshotAsync();
+
+    await expect(async () => {
+      return "async snapshot #2";
+    }).toMatchSpeechSnapshotAsync();
+  });
+});
+
+describe("custom inline snapshot matcher", () => {
+  it("works if sync", () => {
+    expect(() => {
+      return "sync snapshot #1";
+    }).toMatchSpeechInlineSnapshot(`"sync snapshot #1"`);
+
+    expect(() => {
+      return "sync snapshot #2";
+    }).toMatchSpeechInlineSnapshot(`"sync snapshot #2"`);
+  });
+
+  it("works if async", async () => {
+    await expect(async () => {
+      return "async inline snapshot #1";
+    }).toMatchSpeechInlineSnapshotAsync(`"async inline snapshot #1"`);
+    await expect(async () => {
+      return "async inline snapshot #2";
+    }).toMatchSpeechInlineSnapshotAsync(`"async inline snapshot #2"`);
+  });
+});
